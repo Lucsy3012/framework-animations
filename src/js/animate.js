32edf0dd4dfcfc14122baf2134eab2eda6d21c69
Lucas Dietrich <lucas.dietrich@gmx.de>
2019-05-26 18:22:31 +0200

    added 'sequence-sliced' and '.slideUpFade'

--- src/js/animate.js
+++ src/js/animate.js
@@ -15,12 +15,27 @@ $(document).ready(function () {
                 windowHeight = $window.height(),
                 scrolled = $window.scrollTop(),
 
-                _visible = thisPosition <= ((windowHeight * 0.75) + scrolled);
+                _visible = thisPosition <= ((windowHeight * 0.8) + scrolled);
 
             if (_visible) {
                 $this.addClass('in');
             }
         });
     }
+
+    // Slide up sequence
+    var $slicedSequence = $('.sequence-sliced');
+
+    $slicedSequence.each(function() {
+        var $this = $(this),
+            text = $this.text().split('');
+
+        $this.html('');
+
+        for (var i=0; i < text.length; i++) {
+            var span = '<span>' + text[i] + '</span>';
+            $this.append(span);
+        }
+    });
 
 });
